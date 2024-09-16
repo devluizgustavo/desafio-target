6ee579ab9f82286655cbe6a262013557b50e033b
Luiz Gustavo <luizgustavobonke@gmail.com>
2024-09-16 16:36:18 -0300

    Implementação da Lógica Indicada Com o Resultado da Soma

---
+++ scripts/resultSum.js
@@ -0,0 +1,18 @@
+import showMessage from "./showMessage.js";
+/*
+int INDICE = 13, SOMA = 0, K = 0; enquanto K < INDICE faça { K = K + 1; SOMA = SOMA + K; } imprimir(SOMA);
+*/
+
+
+let ind = 10, sum = 0, k = 0;
+
+while (k < ind) {
+    k = k + 1;
+    sum += k;
+}
+
+export default function resultSum() {
+    document.addEventListener('DOMContentLoaded', e => {
+        showMessage(`O resultado da soma é ${sum}`, '#res-sum', true);
+    })
+}
